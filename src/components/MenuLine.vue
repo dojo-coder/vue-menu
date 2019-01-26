a49bc4348e80585be4b84585e60365dc2a299c0e
selucaciu <42737302+selucaciu@users.noreply.github.com>
2019-01-27 00:34:45 +0200

    wip
    wip

--- src/components/MenuLine.vue
+++ src/components/MenuLine.vue
@@ -1,11 +1,6 @@
 <template>
   <svg :class="item.id" :width="size" viewBox="0 0 27 27">
-    <polygon
-      :fill="color"
-      :id="item.id"
-      :points="item.morph.begin"
-      ref="poly"
-    ></polygon>
+    <polygon :fill="color" :id="item.id" ref="poly"></polygon>
   </svg>
 </template>
 
