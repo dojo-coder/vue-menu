b26a99cb36f0963d1b10accf7446ef9cf6b3294f
selucaciu <42737302+selucaciu@users.noreply.github.com>
2019-01-26 23:59:02 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,104 @@
+<template>
+  <div
+    :style="{ width: size + 'px' }"
+    class="menu"
+    @mouseenter="onMouseEnter"
+    @mouseleave="onMouseLeave"
+  >
+    <MenuLine
+      v-for="item in items"
+      :key="item.id"
+      ref="polys"
+      :item="item"
+      :color="color"
+      :size="size"
+    ></MenuLine>
+  </div>
+</template>
+
+<script>
+import MenuLine from "./components/MenuLine";
+import { TweenMax } from "gsap";
+
+export default {
+  name: "App",
+  data: function() {
+    return {
+      size: 80,
+      duration: 0.3,
+      color: "black",
+      items: [
+        {
+          id: "menu-btn-top",
+          morph: {
+            begin:
+              "13.6,0 13.4,0 13.2,0.1 13,0.2 0.6,6.4 0.4,6.6 0.2,6.8 0.1,7.2 0.1,7.5 0.2,7.8 0.4,8 0.6,8.2 12.9,14.9 13,15 13.2,15 13.3,15 13.5,15 13.7,15 13.9,14.9 19.9,11.7 18.8,10 13.4,12.9 3.2,7.3 13.6,2.1 23.8,7.3 17.9,10.5 18.9,12.2 26.4,8.2 26.6,8 26.7,7.8 26.9,7.5 26.9,7.3 26.9,7 26.8,6.8 26.6,6.5 26.4,6.4 14,0.1 13.8,0.1 13.7,0",
+            end:
+              "13.6,6 12.6,6 11.6,6 10.2,6 0.5,6 0,6 0,6.4 0,6.9 0,7.2 0,7.5 0,8 0.5,8 10.2,8 11.4,8 12.2,8 12.9,8 13.5,8 15.2,8 17,8 19,8 18.6,6.9 13.4,6.8 3.2,7 13.6,7.5 23.8,7 17.6,6.9 18,8 26.5,8 27,8 27,7.6 27,7.3 27,7 27,6.7 27,6.4 27,6 26.5,6 17,6 15.7,6 14.6,6"
+          }
+        },
+        {
+          id: "menu-btn-middle",
+          morph: {
+            begin:
+              "3.3,11.3 5.4,12.4 3.2,13.5 13.4,19 23.8,13.4 21.8,12.3 23.8,11.2 26.3,12.5 26.6,12.8 26.9,13.1 26.9,13.4 26.9,13.7 26.8,14 26.5,14.3 13.9,21 13.7,21.1 13.4,21.1 13.2,21.1 13,21.1 12.9,21 0.6,14.3 0.3,14.1 0.2,13.9 0.1,13.7 0.1,13.5 0.1,13.2 0.1,13 0.3,12.8 0.5,12.6",
+            end:
+              "5.2,12 5.2,14 3.1,12 13.4,12 24,12 23.1,14 23.1,12 26.6,12 27,12 27,12.5 27,13 27,13.4 27,14 26.6,14 16.1,14 14.8,14 13.4,14 12.6,14 11.7,14 10.5,14 0.4,14 0,14 0,13.6 0,13.2 0,13 0,12.7 0,12.4 0,12 0.4,12"
+          }
+        },
+        {
+          id: "menu-btn-bottom",
+          morph: {
+            begin:
+              "3.3,17.1 5.4,18.2 3.2,19.3 13.4,24.8 23.8,19.2 21.8,18.1 23.8,17 26.3,18.3 26.6,18.6 26.9,18.9 26.9,19.2 26.9,19.5 26.8,19.8 26.5,20.1 13.9,26.8 13.7,26.9 13.4,26.9 13.2,26.9 13,26.9 12.9,26.8 0.6,20.1 0.3,19.9 0.2,19.7 0.1,19.5 0.1,19.3 0.1,19 0.1,18.8 0.3,18.6 0.5,18.4",
+            end:
+              "5.2,18 5.2,20 3.1,18 13.5,18 24,18 23.1,20 23.1,18 26.6,18 27,18 27,18.5 27,19 27,19.4 27,20 26.6,20 16.1,20 14.8,20 13.5,20 12.6,20 11.8,20 10.5,20 0.4,20 0,20 0,19.6 0,19.2 0,19 0,18.7 0,18.4 0,18 0.4,18"
+          }
+        }
+      ]
+    };
+  },
+  components: {
+    MenuLine
+  },
+  methods: {
+    onMouseEnter: function(e) {
+      this.$refs.polys.forEach((item, index) => {
+        TweenMax.to(item.$el.firstChild, this.duration, {
+          morphSVG: this.items[index].morph.end
+        });
+      });
+    },
+    onMouseLeave: function(e) {
+      this.$refs.polys.forEach((item, index) => {
+        TweenMax.to(item.$el.firstChild, this.duration, {
+          morphSVG: this.items[index].morph.begin
+        });
+      });
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.menu {
+  position: relative;
+  cursor: pointer;
+  display: block;
+  margin: 5% auto 0 auto;
+}
+.menu-btn-middle,
+.menu-btn-bottom {
+  position: absolute;
+  top: 0px;
+  left: 0;
+}
+</style>
---
+++ src/components/MenuLine.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg :class="item.id" :width="size" viewBox="0 0 27 27">
+    <polygon
+      :fill="color"
+      :id="item.id"
+      :points="item.morph.begin"
+      ref="poly"
+    ></polygon>
+  </svg>
+</template>
+
+<script>
+export default {
+  props: ["item", "color", "size"]
+};
+</script>
+
+<style></style>
